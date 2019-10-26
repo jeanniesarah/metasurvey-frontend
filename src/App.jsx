3f0174d64f89052bac47cfdb5c601f249da838d7
v.martynenko <v.martynenko@semrush.com>
2019-10-26 13:23:10 +0300

    remove unusable code

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,6 @@ import Mobile from './components/Mobile';
 import axios from 'axios';
 
 const App = () => {
-  axios
-    .get('https://jsonplaceholder.typicode.com/posts/1')
-    .then(response => JSON.parse(response))
-    .then(data => console.log(data));
-
   return (
     <>
       <Desktop />
