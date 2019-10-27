a3bb84bbfefede210f256d7969a52231b1e2414d
Martyn <zogacc@gmail.com>
2019-10-27 11:47:26 +0300

    minor markup improvements

--- src/components/Desktop/index.jsx
+++ src/components/Desktop/index.jsx
@@ -7,12 +7,12 @@ import logoSrc from 'components/logo.png';
 const Desktop = ({ surveys, onSave }) => {
   if (!surveys) return null;
 
-  const { title } = surveys;
+  const { title, logo } = surveys;
 
   return (
     <div className={styles.Desktop}>
       <div className={styles.LogoBlock}>
-        <img src={logoSrc} alt="MetaSurvey logo" />
+        <img src={logo ? logo : logoSrc} alt="Branding" />
         <h2 className={styles.Title}>{title}</h2>
       </div>
       <Survey surveys={surveys} onSave={onSave} />
