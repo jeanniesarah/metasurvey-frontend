5285231055adb6a3292c1359d378b39f09ece182
Alex Belozerov <asbelozerov@gmail.com>
2019-11-24 22:05:35 +0300

    Alert after save - fix

--- src/components/Mobile/Card/index.jsx
+++ src/components/Mobile/Card/index.jsx
@@ -74,7 +74,7 @@ export default props => {
                 data.slice(1)
               );
 
-              onSave(JSON.stringify(answer));
+              onSave(answer);
             }}
           >
             Submit
